a37a468bb224a93ac905389bd52cbaaeccfd8c8a
nastialyash <user251@school22.rv.ua>
2024-06-18 23:13:33 +0300

    project2

---
+++ main2.py
@@ -0,0 +1,24 @@
+# a = int(input("Enter your number"))
+# b = int(input("Enter your number"))
+# print("Sum numbers")
+# sum = 0
+# count = 0
+# for i in range(a,b):
+#     if i%2 ==0:
+#      sum+=i
+#      count+=1
+#     elif sum+=0
+#         count+=1
+#     if i%9 ==0:
+
+      
+a = int(input("Enter your number"))
+if a>0:
+   print("NNumber is positive")
+elif a<0:
+   print("Number is negative")
+else: 
+   print("Number is equal to zero")
+if a ==7:
+   print("Good bye!")
+        
