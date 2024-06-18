4d34a2706ec4ec106b03ce91d7ec9d758b34eda5
nastialyash <user251@school22.rv.ua>
2024-06-18 23:20:49 +0300

    project2

--- main2.py
+++ main2.py
@@ -1,15 +1,15 @@
-# a = int(input("Enter your number"))
-# b = int(input("Enter your number"))
-# print("Sum numbers")
-# sum = 0
-# count = 0
-# for i in range(a,b):
-#     if i%2 ==0:
-#      sum+=i
-#      count+=1
-#     elif sum+=0
-#         count+=1
-#     if i%9 ==0:
+ a = int(input("Enter your number"))
+ b = int(input("Enter your number"))
+ print("Sum numbers")
+ sum = 0
+ count = 0
+ for i in range(a,b):
+     if i%2 ==0:
+      sum+=i
+      count+=1
+     elif sum+=0
+          count+=1
+    if i%9 ==0:
 
       
 a = int(input("Enter your number"))
